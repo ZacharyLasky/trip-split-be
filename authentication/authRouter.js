c283415f8720530a5d4d06de045194a084a939a4
Zach Lasky <zacharylasky123@gmail.com>
2019-11-25 14:55:32 -0500

    changed info to give name without message on signin

--- authentication/authRouter.js
+++ authentication/authRouter.js
@@ -50,9 +50,7 @@ router.post("/login", (req, res) => {
       const userId = user.id;
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
-        res
-          .status(200)
-          .json({ message: `Welcome ${user.name}`, token, userId });
+        res.status(200).json({ name: user.name, token, userId });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
